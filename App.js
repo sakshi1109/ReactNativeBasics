47d39a864a9fe0fb7fdd24ecc2442dc36b1f28d5
Shubhanshu Jain <shubhanshu.jain@girnanrsoft.co.in>
2023-04-24 02:27:07 +0530

    Added layout with flexes using view component

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-    <View style={styles.inputContainer}>
-      <TextInput style={styles.textInput} placeholder="Your course goal"></TextInput>
-      <Button title="click me"></Button>
-    </View>
-    <View>
-      <Text>List of Goals</Text>
-    </View>
+    <View
+      style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-around',
+        alignItems: 'flex-end',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: 'grey',
-    width: '80%',
-    marginRight: 8,
-    padding: 8
-  }
-  
- 
-});
