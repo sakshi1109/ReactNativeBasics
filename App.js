6f9ae6e6bbbae28565cc28b52cf4bb682ac2e091
Shubhanshu Jain <shubhanshu.jain@girnanrsoft.co.in>
2023-04-24 02:07:47 +0530

    Styling with button and inputtext

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
-defalut ui;
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} placeholder="Your course goal"></TextInput>
+      <Button title="click me"></Button>
+    </View>
+    <View>
+      <Text>List of Goals</Text>
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
+  
+ 
+});
