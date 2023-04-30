6b78c640283d6c9bade8aa55d3e16044d2254cdb
Shubhanshu Jain <shubhanshu.jain@girnanrsoft.co.in>
2023-04-30 22:58:50 +0530

    Dynamic features added

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList} from 'react-native';
+
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText]=useState("")
+  const [courseGoals, setCourseGoals]=useState([]);
+  
+  function goalInputHandler(entredText){
+    setEnteredGoalText(entredText)
+   }
+  function addGoalHandler(){
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
     <View style={styles.inputContainer}>
-      <TextInput style={styles.textInput} placeholder="Your course goal"></TextInput>
-      <Button title="click me"></Button>
+      <TextInput style={styles.textInput} placeholder="Your course goal" onChangeText={goalInputHandler}></TextInput>
+      <Button title="click me" onPress={addGoalHandler}></Button>
     </View>
-    <View>
-      <Text>List of Goals</Text>
+    <View style={styles.goalsContainer}>
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => {
+          
+          return (
+            <View style={styles.goalItems}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
+            </View>
+          );
+        }}
+
+      alwaysBounceVertical={false}/>
     </View>
     </View>
   );
@@ -17,19 +43,38 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey'
   },
   textInput: {
     borderWidth: 1,
     borderColor: 'grey',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  goalItems: {
+    margin: 8,
+    padding: 8, 
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    
+  },
+  goalText: {
+    color: 'white'
   }
-  
  
 });
